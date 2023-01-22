5f7e542d43166ebac9dcf9d63e94acb86be5dfb9
Enova <17408285+Enovale@users.noreply.github.com>
2023-01-22 15:14:45 -0600

    Revert "Use ReactiveUI"
    This reverts commit 699c0448a3a08a6442acb6194d229e084c47568d.

--- Mvvm/ViewModelBase.cs
+++ Mvvm/ViewModelBase.cs
@@ -1,9 +1,14 @@
 ﻿using System.ComponentModel;
-using ReactiveUI;
 
 namespace Glitonea.Mvvm
 {
-    public class ViewModelBase : ReactiveObject
+    public class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
